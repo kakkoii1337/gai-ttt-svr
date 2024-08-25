078887212586474d2a806902bc435af4edd5e65d
RoyLai-InfoCorp <roy.lai@infocorp.io>
2024-08-25 19:47:39 +0800

    Refactored task scripts

---
+++ .vscode/docker_run.py
@@ -0,0 +1,54 @@
+import toml,os,subprocess
+from gai.scripts._scripts_utils import _get_version
+
+base_name="gai-ttt"
+
+# ## Update version in pyproject.toml
+# def __update_version(pyproject_path):
+#     with open(pyproject_path, "r+") as f:
+#         data = toml.load(f)
+
+#         # Extract and update the version number
+#         version_parts = data["tool"]["poetry"]["version"].split(".")
+#         version_parts[-1] = str(int(version_parts[-1]) + 1)  # Increment the patch version
+#         data["tool"]["poetry"]["version"] = ".".join(version_parts)
+
+#         # Write the updated data back to pyproject.toml
+#         f.seek(0)
+#         f.write(toml.dumps(data))
+#         f.truncate()  # Ensure file is truncated if new data is shorter
+
+#         return data["tool"]["poetry"]["version"]
+
+def _docker_run_ttt(component,version="latest"):
+    if component != "gai-ttt":
+        print("Wrong component found.")
+        return
+    cmd=f"""docker run -d \
+        -e DEFAULT_GENERATOR="ttt-exllamav2-mistral7b" \
+        -e SWAGGER_URL="/doc" \
+        -e SELF_TEST="true" \
+        --gpus all \
+        -v ~/.gai:/app/.gai \
+        -p 12031:12031 \
+        --name {component} \
+        --network gai-sandbox \
+        kakkoii1337/{component}:{version}"""
+    os.system(f"docker stop {component} && docker rm -f {component}")
+    os.system(cmd)
+
+def main():
+    here=os.path.dirname(__file__)
+    pyproject_dir=os.path.join(here,'..')
+    dockerfile_dir=pyproject_dir
+    pyproject_path=os.path.join(pyproject_dir,'pyproject.toml')
+
+    # Get the version from the pyproject.toml file
+    version = _get_version(pyproject_path=pyproject_path)
+    print(f"Running gai-ttt:{version}")
+
+    # Exec the docker image
+    _docker_run_ttt(component="gai-ttt",version=version)
+
+if __name__ == "__main__":
+    main()
--- src/gai/ttt/server/api/self-test.py
+++ src/gai/ttt/server/api/self-test.py
@@ -3,19 +3,20 @@ import os,sys,subprocess
 import importlib
 from gai.lib.common.color import yellow,green,red
 verbose=False
+import pkg_resources
 
 def check_package_installed(package_name, err_message=None):
     # Try to find the package
-    package_spec = importlib.util.find_spec(package_name)
+    package = pkg_resources.get_distribution(package_name)
    
-    if package_spec is None:
+    if package is None:
         # Package is not found, handle error
         if err_message:
             red(err_message)  # Print error message if provided
         sys.exit(1)
     else:
         # Package is found, print confirmation message
-        green(f"{package_name} is installed")
+        green(f"{package.project_name} version {package.version} is installed")
 
 def check_package_directory(package_dir, err_message=None):
     import site
@@ -40,15 +41,14 @@ def check_package_directory(package_dir, err_message=None):
 
 def self_test():
 
-    # Check if packages installed
-    check_package_installed("gai-ttt-svr", "gai-ttt-svr is not installed.")
+    # Check if packages installed    
     check_package_installed("gai-sdk", "gai-sdk is not installed. gai-sdk may be missing in 'tool.poetry.dependencies'.")
     
     # Check package directories
-    check_package_directory("gai","site-packages/gai does not exists.")
-    check_package_directory("gai/ttt","site-packages/gai/ttt does not exists.")
-    check_package_directory("gai/ttt/server","site-packages/gai/ttt/server does not exists.")
-    check_package_directory("gai/ttt/server/api","site-packages/gai/ttt/server/api does not exists. Check if gai-ttt-svr/setup.py has packaged the files correctly.")
+    # check_package_directory("gai","site-packages/gai does not exists.")
+    # check_package_directory("gai/ttt","site-packages/gai/ttt does not exists.")
+    # check_package_directory("gai/ttt/server","site-packages/gai/ttt/server does not exists.")
+    # check_package_directory("gai/ttt/server/api","site-packages/gai/ttt/server/api does not exists. Check if gai-ttt-svr/setup.py has packaged the files correctly.")
     
     green("self_test: OK")
     exit(0)
